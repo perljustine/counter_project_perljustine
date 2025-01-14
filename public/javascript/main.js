64974b6c0a5f85fc45b974bf1705d36753e98cab
perljustine <justine.perl@hotmail.com>
2025-01-14 10:13:48 +0100

    fin creation counter

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,34 +2,36 @@ let body = document.querySelector('body');
 let Montitre = document.createElement('h1');
 let textemontitre = document.createTextNode ('Counter');
 let div = document.createElement('div');
+
 body.appendChild(div);
 
 Montitre.appendChild(textemontitre);
 div.appendChild(Montitre);
 
-let counterlabel = document.createElement ('label');
+let Counterlabel = document.createElement ('label');
 let textecounterlabel = document.createTextNode (0);
-counterlabel.appendChild(textecounterlabel);
-div.appendChild(counterlabel);
+Counterlabel.appendChild(textecounterlabel);
+div.appendChild(Counterlabel);
 
-let counter = 0;
-
-let countermoins = document.createElement('button');
+let Countermoins = document.createElement('button');
 let textemoncountermoins = document.createTextNode ('-');
 
-countermoins.appendChild(textemoncountermoins); 
-body.appendChild(countermoins);
+Countermoins.appendChild(textemoncountermoins); 
+body.appendChild(Countermoins);
 
-let reset = document.createElement('button');
+let Reset = document.createElement('button');
 let textereset= document.createTextNode ('reset');
 
-reset.appendChild(textereset); 
-body.appendChild(reset);
+Reset.appendChild(textereset); 
+body.appendChild(Reset);
 
-let counterplus = document.createElement('button');
+let Counterplus = document.createElement('button');
 let textemoncounterplus = document.createTextNode ('+');
 
-counterplus.appendChild(textemoncounterplus); 
-body.appendChild(counterplus);
+Counterplus.appendChild(textemoncounterplus); 
+body.appendChild(Counterplus);
+
+
+
 
 
